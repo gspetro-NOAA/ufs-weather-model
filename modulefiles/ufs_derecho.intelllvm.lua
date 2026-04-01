32f40af456b2ec5c65f8a6af473e6d6e07e98aa2
Gillian Petro <gillian.petro@noaa.gov>
2026-04-01 14:59:36 -0400

    rename & update derecho intel modulefile

--- modulefiles/ufs_derecho.intelllvm.lua
+++ modulefiles/ufs_derecho.intelllvm.lua
@@ -2,16 +2,24 @@ help([[
 loads UFS Model prerequisites for NOAA Parallelworks/Intel
 ]])
 
+purge(true)
+
 setenv("LMOD_TMOD_FIND_FIRST","yes")
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/cray-mpich/8.1.29-3sepg3g/gcc/12.4.0")
 
-unload("ncarcompilers")
-stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
-load(pathJoin("stack-oneapi", stack_intel_ver))
+prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-2.1.0/envs/ue-oneapi-2025.3.1/modules/Core")
+prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/installs/oneapi-2025.3.1/modulefiles")
+prepend_path("MODULEPATH", "/opt/cray/pe/modulefiles")
 
-stack_cray_mpich_ver=os.getenv("stack-cray-mpich_ver") or "8.1.29"
+load("crayenv/25.03")
+-- unload("ncarcompilers")
+
+stack_intel_ver=os.getenv("stack_intel_ver") or "2025.3.1"
+load(pathJoin("stack-intel-oneapi-compilers", stack_intel_ver))
+
+stack_cray_mpich_ver=os.getenv("stack-cray-mpich_ver") or "8.1.32"
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
+
+unload("cray-libsci")
 
 cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
