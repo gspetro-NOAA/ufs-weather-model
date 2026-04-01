7de85131179795761767b59e844cdc96949947ee
Gillian Petro <gillian.petro@noaa.gov>
2026-04-01 15:19:51 -0400

    update hercules modulesfiles for ss2.x

--- modulefiles/ufs_hercules.intelllvm.lua
+++ modulefiles/ufs_hercules.intelllvm.lua
@@ -2,13 +2,13 @@ help([[
 loads UFS Model prerequisites for Hercules/IntelLLVM
 ]])
 
-prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.1.0/install/modulefiles/intel-oneapi-mpi/2021.13-sqiixt7/gcc/13.3.0")
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-2.1.0/envs/ue-oneapi-2025.3.1/modules/Core")
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/modulefiles")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
-load(pathJoin("stack-oneapi", stack_intel_ver))
+stack_intel_ver=os.getenv("stack_intel_ver") or "2025.3.1"
+load(pathJoin("stack-intel-oneapi-compilers", stack_intel_ver))
 
-stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.17"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.27.9"
