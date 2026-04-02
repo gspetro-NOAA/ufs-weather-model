5c24bf1a253c32ff73a150ccf6caa31edd8cab82
Gillian Petro <gillian.petro@noaa.gov>
2026-04-01 21:27:00 -0400

    update acorn modulefile info

--- modulefiles/ufs_acorn.intel.lua
+++ modulefiles/ufs_acorn.intel.lua
@@ -2,10 +2,10 @@ help([[
 Load environment to build UFS on Acorn with Intel compiler
 ]])
 
-prepend_path("MODULEPATH", "/lfs/h1/emc/nceplibs/noscrub/spack-stack/spack-stack-1.9.2/envs/ue-intel-19/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/lfs/h1/emc/nceplibs/noscrub/spack-stack/spack-stack-2.1.0/envs/ue-oneapi-2024.2.1/modules/Core")
 
-load("stack-intel")
-load("stack-cray-mpich")
+load("stack-intel-oneapi-compilers/2024.2.1") -- or load("stack-intel-oneapi-compilers") ?
+load("stack-cray-mpich/8.1.29") -- or load("stack-cray-mpich") ?
 
 --Avoid production installations; use spack-stack only:
 remove_path("MODULEPATH", "/apps/ops/prod/libs/modulefiles/compiler/intel/19.1.3.304")
