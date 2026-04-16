0e7486366bad50315d9b9527f976d5fd3bacef9c
Gillian Petro <gillian.petro@noaa.gov>
2026-04-16 15:39:04 -0400

    revert derecho intel modulefile

--- modulefiles/ufs_derecho.intel.lua
+++ modulefiles/ufs_derecho.intel.lua
@@ -1,15 +1,16 @@
 help([[
-loads UFS Model prerequisites for Derecho/GNU
+loads UFS Model prerequisites for NOAA Parallelworks/Intel
 ]])
 
 setenv("LMOD_TMOD_FIND_FIRST","yes")
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-gcc-12.4/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/cray-mpich/8.1.29-3sepg3g/gcc/12.4.0")
 
 unload("ncarcompilers")
-stack_gnu_ver=os.getenv("stack_gnu_ver") or "12.4.0"
-load(pathJoin("stack-gcc", stack_gnu_ver))
+stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
+load(pathJoin("stack-oneapi", stack_intel_ver))
 
-stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.29"
+stack_cray_mpich_ver=os.getenv("stack-cray-mpich_ver") or "8.1.29"
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.27.9"
@@ -26,6 +27,9 @@ load(pathJoin("nccmp", nccmp_ver))
 setenv("CC", "mpicc")
 setenv("CXX", "mpicxx")
 setenv("FC", "mpif90")
-setenv("CMAKE_Platform", "derecho.gnu")
+setenv("I_MPI_CC", "icx")
+setenv("I_MPI_CXX", "icpx")
+setenv("I_MPI_F90", "ifort")
 
+setenv("CMAKE_Platform", "derecho.intel")
 whatis("Description: UFS build environment")
