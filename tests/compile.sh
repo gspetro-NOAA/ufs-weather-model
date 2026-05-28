4a9619fce74da923edd74e5e4c2afffc39c41f32
Gillian Petro <gillian.petro@noaa.gov>
2026-05-28 12:57:08 -0400

    fix linter error

--- tests/compile.sh
+++ tests/compile.sh
@@ -57,7 +57,7 @@ BUILD_JOBS=${BUILD_JOBS:-8}
 set +x
 case ${MACHINE_ID} in
   macosx|linux)
-    # shellcheck source=/github/workspace/modulefiles/ufs_ursa.intelllvm.lua
+    # shellcheck source=/github/workspace/modulefiles/ufs_ursa.oneapi.lua
     source "${PATHTR}/modulefiles/ufs_${MACHINE_ID}.${RT_COMPILER}"
     ;;
   *)
