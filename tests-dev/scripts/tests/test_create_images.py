00622a101111579357923e6303eeeffdbc2ef03a
Gillian Petro <gillian.petro@noaa.gov>
2026-07-31 17:23:51 -0400

    initial commit & plotting bugfix

---
+++ tests-dev/scripts/tests/test_create_images.py
@@ -0,0 +1,127 @@
+from pathlib import Path
+import pytest
+import requests
+import numpy as np
+from scripts.get_data import *
+from scripts.create_images import *
+
+@pytest.mark.parametrize('category', ['runtime', 'memory'])
+def initialize_PlotManager(test_data, category):
+   """Check that PlotManager is initialized properly."""
+   plot_manager = PlotManager(test_data,category)
+   assert plot_manager.data == test_data
+   assert plot_manager.category == category
+
+@pytest.mark.parametrize('category', ['runtime', 'memory'])
+def test_get_test_names(test_data, category):
+   """Check that get_test_names() extracts all test names by comparing with rt.conf.
+   """
+
+   plot_manager = PlotManager(test_data,category)
+
+   response = requests.get('https://raw.githubusercontent.com/ufs-community/ufs-weather-model/refs/heads/develop/tests/rt.conf')
+   response = response.text.splitlines()
+   tests = set()
+
+   compiler = ''
+   for line in response:
+      if line.startswith('COMPILE'):
+         compiler = line.split("|")[2].strip()
+      elif line.startswith('RUN'):
+         tests.add(line.split('|')[1].strip() + f"_{compiler}")
+   
+   assert tests == plot_manager.get_test_names()
+
+@pytest.mark.parametrize('category', ['runtime', 'memory'])
+def test_organize_data_by_test(test_data, data_by_test, category):
+   """Check that organize_data_by_test() creates new dictionaries that use test name as primary key instead of machine as primary key. 
+   """
+   plot_manager = PlotManager(test_data,category)
+   actual_data_by_test = plot_manager.organize_data_by_test()
+   expected_data_by_test = data_by_test
+   
+   for test in expected_data_by_test:
+      for machine in ['hera', 'ursa']:
+         try:
+            assert expected_data_by_test[test][category][machine] == actual_data_by_test[test][category][machine]
+         except KeyError:
+            continue
+
+def test_detect_statistical_anomalies(test_data):
+
+   data = [2091, 1195, 2699, 1896, 2098, 2712, 2249, 1620, 1938, 1132, 1978, 1215, 1523, 2257, 1852, 1184, 1541, 1803, 2004, 1962, 2030, 2680, 1306, 1471, 2292, 1740, 2831, 1746, 1255, 1668]
+
+   plot_manager = PlotManager(test_data,'memory')
+
+   actual_anomalies = plot_manager.detect_statistical_anomalies(data)
+   mean = 1865.6
+   stdev = 474.02543
+   expected_anomalies = []
+
+   for num in data: 
+      if num > mean + 2 * stdev:
+         expected_anomalies.append(True)
+      else:
+         expected_anomalies.append(False)
+
+   assert actual_anomalies == expected_anomalies
+
+def test_rearrange_hashes(test_data, hashes):
+   plot_manager = PlotManager(test_data,'runtime')
+   plot_manager.hashes = plot_manager.rearrange_hashes()
+   hashes = ['PR Head'] + get_hashes()
+   hashes.reverse()
+   assert plot_manager.hashes == hashes
+
+@pytest.mark.parametrize('category', ['runtime', 'memory'])
+def test_generate_figure(test_data, category, hashes):
+   plot_manager = PlotManager(test_data,category)
+   plot_manager.hashes = hashes
+   plot = plot_manager.generate_figure("cpld_sample_test")
+   
+   assert plot.gca().get_title() == f"{category} for cpld_sample_test"
+   assert plot.gca().get_ylabel() == category
+   assert plot.gca().get_xlabel() == "Commit Hash: oldest --> newest"
+
+@pytest.mark.parametrize('category', ['runtime', 'memory'])
+@pytest.mark.parametrize('test', ['cpld_control_pdlib_p8_gnu', 'cpld_debug_pdlib_p8_gnu',
+                         'datm_cdeps_control_cfsr_gnu', 'control_gfs_mpas_gnu', 
+                         'pm_ideal_supercell_intel', 'control_p8_intel',
+                         'control_p8_ugwpv1_tempo_aerosol_hail_intel'])
+def test_add_test_metrics_by_machine(test_data_subset, data_by_test, category, hashes, test):
+   """Test hash retrieval and metrics restructuring prior to plotting."""
+   plot_manager = PlotManager(test_data_subset, category)
+   plot_manager.metrics = plot_manager.organize_data_by_test()
+   plot_manager.hashes = hashes[-11:]
+   plot = plot_manager.generate_figure(test)
+   plot = plot_manager.add_test_metrics_by_machine(plot, test)
+   lines = plot.gca().get_lines()
+   
+   for line in lines:
+      if not line.get_label().startswith("_child"): #anomaly points are labeled "_child#"
+         expected_data = data_by_test[test][category][line.get_label()]
+         expected_data.reverse()
+         actual_data = line.get_ydata()
+         assert np.array_equal(actual_data, expected_data)
+      
+
+
+@pytest.mark.run_manual
+@pytest.mark.parametrize('category', ['runtime', 'memory'])
+def test_plot_results(test_data_subset, category):
+   """
+   Check that plotting runs error-free and generates expected files. 
+   """
+
+   # Delete files in plots dir first?
+   plot_manager = PlotManager(test_data_subset, category)
+   plot_manager.plot_results()
+
+   tests = ['cpld_control_pdlib_p8_gnu', 'cpld_debug_pdlib_p8_gnu',
+            'datm_cdeps_control_cfsr_gnu', 'control_gfs_mpas_gnu', 
+            'pm_ideal_supercell_intel', 'control_p8_intel',
+            'control_p8_ugwpv1_tempo_aerosol_hail_intel', ]
+
+   for test in tests:
+      filepath = Path(f"plots/{test}_{category}.png")
+      assert filepath.is_file(), f"File not found: {filepath}"
--- tests-dev/scripts/tests/test_write_test_summary.py
+++ tests-dev/scripts/tests/test_write_test_summary.py
@@ -1,23 +1,54 @@
+import os
 from mdutils.mdutils import MdUtils
 import pandas as pd
 from scripts.write_test_summary import *
-from scripts.write_test_summary import _count_passes_per_machine, _count_passes_per_test
 
-def test_load_json(stats_dict_snippet):
+def test_init_HTMLBuilder(set_env_vars):
 
-   content = load_json('test_file_stats.json')
-   assert stats_dict_snippet == content
+   set_env_vars
+   html_builder = HTMLBuilder()
+   assert html_builder.mdFile.get_md_text() == "\nTest Summary for PR #2882\n=========================\n"
+   assert html_builder.mdFile.file_name == 'summary.md'
+   assert len(html_builder.repo_hashes) == 30
+   assert html_builder.machines == os.environ.get("MACHINES").split()
+   assert html_builder.categories == ["runtime", "memory"]
 
-def test_create_mdFile():
+def test_set_mdFile(set_env_vars, monkeypatch):
 
-   mdFile = create_mdFile()
-   assert mdFile.get_md_text() == "\nTest Summary for PR #2882\n=========================\n"
-   assert mdFile.file_name == 'summary.md'
+   set_env_vars
+   html_builder = HTMLBuilder()
+   assert html_builder.mdFile.get_md_text() == "\nTest Summary for PR #2882\n=========================\n"
+   monkeypatch.setenv("PR_NUM", "3207")
+   html_builder.set_mdFile()
+   assert html_builder.mdFile.get_md_text() == "\nTest Summary for PR #3207\n=========================\n"
+   assert html_builder.mdFile.file_name == 'summary.md'
+   
+def test_get_mdFile(set_env_vars, monkeypatch):
 
-def test_build_content(sample_runtime_results, actual_passes_per_test, actual_passes_per_machine):
+   set_env_vars
+   monkeypatch.setenv("PR_NUM", "3190")
+   html_builder = HTMLBuilder()
+   assert html_builder.get_mdFile().get_md_text() == "\nTest Summary for PR #3190\n=========================\n"
+   assert html_builder.get_mdFile().file_name == 'summary.md'
 
-   os.environ["RUNTIME_RESULTS"] = "runtime_results.json"
-   content = build_content("runtime").sort_index()
+def test_add_legend():
+   
+   html_builder = HTMLBuilder()
+   html_builder.add_legend("memory")
+   predicted_text = "\nTest Summary for PR #2882\n=========================\n\n\n" + "<h4>Key:</h4>\n\n" + \
+   f"&nbsp;&nbsp;&nbsp;&nbsp;✅ = NORMAL memory: Memory falls within two standard deviations of the mean.\n\n" + \
+   f"&nbsp;&nbsp;&nbsp;&nbsp;⚠️ = Memory WARNING: Memory is greater than two standard deviations above the mean.\n\n" + \
+   f"&nbsp;&nbsp;&nbsp;&nbsp;❌ = Memory FAIL: For the past 2+ PRs, memory has been greater than two standard deviations above the mean.\n\n" + \
+   f"&nbsp;&nbsp;&nbsp;&nbsp;N/A = Test does not run on this machine.\n\n\n"
+   assert html_builder.get_mdFile().get_md_text() == predicted_text
+
+def test_build_content(set_env_vars,monkeypatch,sample_runtime_results, actual_passes_per_test, actual_passes_per_machine):
+
+   set_env_vars
+   monkeypatch.setenv("RUNTIME_RESULTS", "runtime_results.json")
+   html_builder = HTMLBuilder()
+   content = html_builder.build_content("runtime").sort_index()
+   print(content)
 
    # Create comparison DataFrame from fixtures
    sample_runtime_results["Passing"] = actual_passes_per_test
@@ -27,30 +58,37 @@ def test_build_content(sample_runtime_results, actual_passes_per_test, actual_pa
 
    assert content.equals(actual_results)
 
-def test_write_content(sample_runtime_results_complete, failing_results_table, actual_passes_per_machine):
+def test_write_content(monkeypatch, set_env_vars, sample_runtime_results_complete, failing_results_table, actual_passes_per_machine):
    """Compare the results of write_content() with a markdown table containing the expected results.
    """
    
    # Set up and test write_content() method
-   mdFile = create_mdFile()
-   os.environ["MACHINES"] = "hercules orion ursa"
+   set_env_vars
+   monkeypatch.setenv("RUNTIME_RESULTS", "runtime_results.json")
+   monkeypatch.setenv("MACHINES", "hercules orion ursa")
+   html_builder = HTMLBuilder()
    results = pd.DataFrame.from_dict(sample_runtime_results_complete).fillna("N/A").sort_index()
    results = pd.concat([results, pd.DataFrame.from_dict(actual_passes_per_machine, orient='index', columns=["hercules","orion","ursa","Passing"])])
-   results = write_content(results, mdFile)
+   html_builder.write_content(results, "runtime")
 
    # Create comparison markdown table with only failing results
    table_header = "\nTest Summary for PR #2882\n=========================\n\n" + \
                   "|Test|hercules|orion|ursa|Passing|\n" + "| :---: | :---: | :---: | :---: | :---: |\n|"
    table_contents =  table_header + failing_results_table + "\n\n\n</details>"
 
-   assert results.get_md_text() == table_contents
+   assert html_builder.get_mdFile().get_md_text() == table_contents
 
-def test_create_summary(failing_results_table):
+def test_create_summary(set_env_vars, monkeypatch, failing_results_table):
    """Compare the results of create_summary() with a markdown string containing the expected results.
    """
    
-   summary_file = create_summary(['runtime'])
-
+   set_env_vars
+   monkeypatch.setenv("RUNTIME_RESULTS", "runtime_results.json")
+   monkeypatch.setenv("MACHINES", "hercules orion ursa")
+   html_builder = HTMLBuilder()
+   html_builder.categories = ["runtime"] # Only create summary for runtime to simplify testing
+   html_builder.create_summary()
+   
    # Create comparison markdown table with only failing results
    table_header = "\nTest Summary for PR #2882\n=========================\n" + \
                   "<details><summary><h3>RUNTIME Results Summary</h3></summary>\n" + \
@@ -62,12 +100,14 @@ def test_create_summary(failing_results_table):
    
    table_contents = table_header + failing_results_table + "\n\n\n</details>"
 
-   assert summary_file.get_md_text() == table_contents
+   assert html_builder.get_mdFile().get_md_text() == table_contents
 
 
 def test_count_passes_per_machine(sample_runtime_results, actual_passes_per_machine):
    """Tests whether the calculated number of tests passing per machine is the same as the actual number of tests passing per machine."""
    
+   html_builder = HTMLBuilder()
+
    # Set up dataframe with test results
    results = pd.DataFrame()
 
@@ -76,7 +116,7 @@ def test_count_passes_per_machine(sample_runtime_results, actual_passes_per_mach
       results = pd.merge(results, machine_results, left_index=True, right_index=True, how='outer').fillna("N/A")
 
    # Calculate passing tests per machine
-   results = _count_passes_per_machine(results)
+   results = html_builder._count_passes_per_machine(results)
    actual_values = pd.DataFrame.from_dict(actual_passes_per_machine, orient='index', columns=["hercules","orion","ursa","Passing"])
    
    assert results.equals(actual_values)
@@ -84,6 +124,8 @@ def test_count_passes_per_machine(sample_runtime_results, actual_passes_per_mach
 def test_count_passes_per_test(sample_runtime_results, actual_passes_per_test):
    """Tests whether the calculated number of tests passing is the same as the actual number of tests passing."""
    
+   html_builder = HTMLBuilder()
+
    # Set up dataframe with test results
    results = pd.DataFrame()
 
@@ -92,7 +134,7 @@ def test_count_passes_per_test(sample_runtime_results, actual_passes_per_test):
       results = pd.merge(results, machine_results, left_index=True, right_index=True, how='outer').fillna("N/A")
 
    # Calculate passing tests
-   results = _count_passes_per_test(results)['Passing']
+   results = html_builder._count_passes_per_test(results)['Passing']
 
    # Sort by index before comparing calculated and actual values for equality
    assert results.sort_index().equals(pd.Series(actual_passes_per_test, name='Passing').sort_index())
